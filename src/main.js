a382b0859b9617fea32b8b7ecc16974df4116295
liulong <1362457117@qq.com>
2019-08-19 14:22:08 +0800

    整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入element组件
-import 'element-ui/lib/theme-chalk/index.css' // 引入样式                              // 引入样式
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入样式
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login">
+        login
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+    .login{
+        width: 100%;
+        height: 100vh;
+        background-color: red;
+    }
+</style>

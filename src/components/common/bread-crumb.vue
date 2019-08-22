5ba7859164e8556913e15f9843432b481b5fdb87
liulong <1362457117@qq.com>
2019-08-22 14:07:06 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,17 @@
+<template>
+<!-- 这是个面包屑，需要注册全局组件 -->
+<el-breadcrumb separator='>'>
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>
+      <!-- 定义具名插槽 -->
+      <slot name="title"></slot>
+  </el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
+// 组件的全局注册
 import leftaside from '../components/home/layout-aside.vue'
 import layouthearder from '../components/home/layout-header.vue'
-
+import breadCrumb from '../components/common/bread-crumb.vue'
 export default {
   install (Vue) {
     Vue.component('layout-aside', leftaside) // 注册左边菜单组件
     Vue.component('layout-header', layouthearder) // 注册右边头部组件
+    Vue.component('bread-crumb', breadCrumb) // 注册全局面包屑
   }
 }

89bca4f1bc02daf18bbcc808fc6dfc3693a4e3f9
liulong <1362457117@qq.com>
2019-08-22 21:38:47 +0800

    评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -23,6 +23,18 @@
          </template>
      </el-table-column>
   </el-table>
+  <!-- 分页功能 -->
+  <el-row type="flex" justify="center" style="margin:20px 0">
+      <el-pagination
+  background
+  layout="prev, pager, next"
+  :current-page="page.currentpage"
+  :page-size="page.pagesize"
+  @current-change='changepage'
+  :total="page.total"
+ >
+</el-pagination>
+  </el-row>
   </el-card>
 </template>
 
@@ -30,10 +42,20 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        pagesize: 10, // 每页多少条
+        total: 0, // 总共多少条
+        currentpage: 1 // 当前页面
+      }
     }
   },
   methods: {
+    // 页码改变时触发    newPage最新页码值
+    changepage (newPage) {
+      this.page.currentpage = newPage // 将最新页码赋值给data中的变量
+      this.getcomments()
+    },
     // 评论开关
     closeOrOpen (row) {
     //   console.log(row)
@@ -53,13 +75,18 @@ export default {
     },
     // 渲染页面
     getcomments () {
+    //   let pageParams = { page: this.page.currentpage, per_page: this.page.pagesize }
       this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment', // 查询评论相关的数据
+          //   ...pageParams
+          page: this.page.currentpage, // 当前页参数
+          per_page: this.page.pagesize // 页码参数
         }
-      }).then(res => {
-        this.list = res.data.results
+      }).then(result => {
+        this.list = result.data.results // 取到列表数据 给 当前的数据对象
+        this.page.total = result.data.total_count // 评论文章总数赋值给当前分页的总数
       })
     },
     // 用来格式化内容    Cellvalue当前单元格值

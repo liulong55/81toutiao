fecb02d4e42cf82af1ac500f9b3ba6c4f9f6fb98
liulong <1362457117@qq.com>
2019-08-22 21:56:49 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,10 +1,27 @@
 <template>
-   <div>素材管理</div>
+   <el-card>
+         <bread-crumb slot="header">
+            <template slot="title">素材管理</template>
+      </bread-crumb>
+      <!--选项卡切换 -->
+       <el-tabs v-model="activeName" >
+    <el-tab-pane label="全部" name="first">
+
+    </el-tab-pane>
+    <el-tab-pane label="收藏" name="second">
+
+    </el-tab-pane>
+  </el-tabs>
+   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'first'
+    }
+  }
 }
 </script>
 

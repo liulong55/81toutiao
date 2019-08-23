61e10292302553e9976d83d0dd0df44367433d61
liulong <1362457117@qq.com>
2019-08-23 21:29:45 +0800

    内容列表-页面结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -24,20 +24,99 @@
         </el-select>
       </el-form-item>
       <el-form-item label="时间选择">
-       <el-date-picker
-      v-model="value1"
-      type="daterange"
-      start-placeholder="开始日期"
-      end-placeholder="结束日期">
-    </el-date-picker>
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
       </el-form-item>
     </el-form>
+    <!--↓↓↓-->
+    <div class="total_title">共找到xxx条评论</div>
+    <!-- 内容项 -->
+    <div class="content-list">
+      <!-- 循环项 -->
+      <div class="content-item" v-for="(item,index) in list" :key="index">
+        <!-- 左侧内容 -->
+        <div class="left">
+          <img src="../../assets/img/icons.png" alt />
+          <!-- 内容信息 -->
+          <div class="info">
+            <span>我是内容标题</span>
+            <el-tag style="width:60px">标签一</el-tag>
+            <span class="date">2019-08-23 20:14:34</span>
+          </div>
+        </div>
+        <!-- 右侧内容 -->
+        <div class="right">
+          <span>
+            <i class="el-icon-edit"></i>
+            修改
+          </span>
+          <span>
+            <i class="el-icon-delete"></i>
+            删除
+          </span>
+        </div>
+      </div>
+    </div>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      list: [1, 2, 3, 5, 6]
+    }
+  }
+}
 </script>
 
-<style>
+<style lang='less' scoped>
+.total_title {
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px dashed #ccc;
+}
+.content-list {
+  .content-item {
+    padding: 10px;
+    border-bottom: 1px solid #f2f3f5;
+    display: flex;
+    justify-content: space-between;
+    .left {
+      display: flex;
+      align-items: center;
+      img {
+        width: 150px;
+        height: 100px;
+        border-radius: 4px;
+      }
+      .info {
+        margin-left: 10px;
+        display: flex;
+        height: 100px;
+        padding: 5px 0;
+        flex-direction: column;
+        justify-content: space-between;
+        .date {
+          color: #999;
+          font-size: 12px;
+        }
+      }
+    }
+    .right {
+      span,
+      span i {
+        font-size: 12px;
+        color: #333;
+      }
+      span {
+        margin-right: 5px;
+      }
+    }
+  }
+}
 </style>

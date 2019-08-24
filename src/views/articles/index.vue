d15c17fb918f063eb1a80448aae968b2cd61be70
liulong <1362457117@qq.com>
2019-08-24 15:14:28 +0800

    内容列表-页面结构-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -6,7 +6,7 @@
     <!-- 工具栏-表单 -->
     <el-form style="margin-left:40px">
       <el-form-item label="文章状态">
-        <el-radio-group v-model="formData.status" @change='refreshList'>
+        <el-radio-group v-model="formData.status" @change="refreshList">
           <!-- label必须有值 -->
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
@@ -16,21 +16,16 @@
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道选择">
-        <el-select v-model="formData.channelid" placeholder="请选择" @change='refreshList'>
-          <el-option
-            v-for="item in channels"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          ></el-option>
+        <el-select v-model="formData.channelid" placeholder="请选择" @change="refreshList">
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="时间选择">
         <!-- value-format 是指定绑定值的格式 -->
         <el-date-picker
-        @change='refreshList'
+          @change="refreshList"
           v-model="formData.dateRange"
-          value-format='yyyy-MM-dd'
+          value-format="yyyy-MM-dd"
           type="daterange"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
@@ -38,9 +33,7 @@
       </el-form-item>
     </el-form>
     <!--↓↓↓-->
-    <div class="total_title">
-       共找到{{page.total}}条文章
-      </div>
+    <div class="total_title">共找到{{page.total}}条文章</div>
     <!-- 内容项 -->
     <div class="content-list">
       <!-- 循环项 -->
@@ -50,10 +43,13 @@
           <img :src="item.cover.images[0]" alt />
           <!-- 内容信息 -->
           <div class="info">
-              <!-- 文章标题 -->
+            <!-- 文章标题 -->
             <span>{{item.title}}</span>
-                                          <!--   过滤类型                  过滤文本           -->
-            <el-tag style="width:60px" :type="item.status | statusType ">{{item.status | statusText}}</el-tag>
+            <!--   过滤类型                  过滤文本           -->
+            <el-tag
+              style="width:60px"
+              :type="item.status | statusType "
+            >{{item.status | statusText}}</el-tag>
             <span class="date">{{item.pubdate}}</span>
           </div>
         </div>
@@ -70,6 +66,10 @@
         </div>
       </div>
     </div>
+    <!-- 分页组件 -->
+    <el-row type="flex" justify="center" style="margin-top:20px">
+      <el-pagination @current-change='changepage' :current-page="page.currentpage" :page-size="page.pagesize" background layout="prev, pager, next" :total="page.total"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -86,14 +86,25 @@ export default {
       channels: [], // 频道数据
       list: [], // 内容列表
       page: {
-        total: 0
+        total: 0, // 总条数
+        pagesize: 10, // 每页数量
+        currentpage: 1 // 第一页
       }
     }
   },
   methods: {
-    // 刷新列表数据  状态改变/频道切换/日期改变 都会触发同一个事件
-    refreshList () {
-      // console.log(this.formData.status) // 看一下是不是最新的状态
+    // 页码改变事件
+    changepage (newPage) {
+      this.page.currentpage = newPage // 获取当前的最新页码
+      // 点击了第二页,但是有条件,所以要把所有条件加在一起
+      // let conditions = this.getConditions() // 获取其他条件
+      // conditions.page = this.page.currentpage // 合并参数 最新页数
+      // conditions.per_page = this.page.pagesize // 合并参数 最新每页数量
+      // this.getArticles(conditions) // 调接口,把值传进去
+      this.getArticles(this.getConditions()) // 查询数据
+    },
+    // 获取条件
+    getConditions () {
       let { status, channelid, dateRange } = this.formData // 把所有的解构出来
       let params = {
         // 三元表达式
@@ -102,6 +113,16 @@ export default {
         begin_pubdate: dateRange && dateRange.length ? dateRange[0] : null,
         end_pubdate: dateRange && dateRange.length > 1 ? dateRange[1] : null
       }
+      params.page = this.page.currentpage
+      params.per_page = this.page.pagesize
+      return params
+    },
+    // 刷新列表数据  状态改变/频道切换/日期改变 都会触发同一个事件
+    refreshList () {
+      // console.log(this.formData.status) // 看一下是不是最新的状态
+      // 当筛选条件改变时,应该将页码回到第一页
+      this.page.currentpage = 1
+      let params = this.getConditions()
       this.getArticles(params) // 在这调用下面的接口,把值传进去
     },
     // 获取文章
@@ -124,7 +145,8 @@ export default {
     }
   },
   // 过滤器,做状态显示的标签
-  filters: { // value是传过来的值
+  filters: {
+    // value是传过来的值
     statusText (value) {
       switch (value) {
         case 0:
@@ -152,7 +174,7 @@ export default {
     }
   },
   created () {
-    this.getArticles() // 获取文章
+    this.getArticles({ page: 1, per_page: 10 }) // 获取文章 // 查询第一页数据
     this.getChannels() // 获取频道
   }
 }

93d9b4dddbb64aabf1fdcc2dd1f1d7bd64c240b3
liulong <1362457117@qq.com>
2019-08-24 14:13:09 +0800

    内容列表-页面结构-搜索筛选

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -3,29 +3,34 @@
     <bread-crumb slot="header">
       <template slot="title">内容列表</template>
     </bread-crumb>
-    <el-form>
+    <!-- 工具栏-表单 -->
+    <el-form style="margin-left:40px">
       <el-form-item label="文章状态">
-        <el-radio-group>
-          <el-radio label="1">全部</el-radio>
-          <el-radio label="2">草稿</el-radio>
-          <el-radio label="3">待审核</el-radio>
-          <el-radio label="4">审核成功</el-radio>
-          <el-radio label="5">审核失败</el-radio>
+        <el-radio-group v-model="formData.status" @change='refreshList'>
+          <!-- label必须有值 -->
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核成功</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道选择">
-        <el-select v-model="value" placeholder="请选择">
+        <el-select v-model="formData.channelid" placeholder="请选择" @change='refreshList'>
           <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
+            v-for="item in channels"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
           ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="时间选择">
+        <!-- value-format 是指定绑定值的格式 -->
         <el-date-picker
-          v-model="value1"
+        @change='refreshList'
+          v-model="formData.dateRange"
+          value-format='yyyy-MM-dd'
           type="daterange"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
@@ -33,19 +38,23 @@
       </el-form-item>
     </el-form>
     <!--↓↓↓-->
-    <div class="total_title">共找到xxx条评论</div>
+    <div class="total_title">
+       共找到{{page.total}}条文章
+      </div>
     <!-- 内容项 -->
     <div class="content-list">
       <!-- 循环项 -->
       <div class="content-item" v-for="(item,index) in list" :key="index">
         <!-- 左侧内容 -->
         <div class="left">
-          <img src="../../assets/img/icons.png" alt />
+          <img :src="item.cover.images[0]" alt />
           <!-- 内容信息 -->
           <div class="info">
-            <span>我是内容标题</span>
-            <el-tag style="width:60px">标签一</el-tag>
-            <span class="date">2019-08-23 20:14:34</span>
+              <!-- 文章标题 -->
+            <span>{{item.title}}</span>
+                                          <!--   过滤类型                  过滤文本           -->
+            <el-tag style="width:60px" :type="item.status | statusType ">{{item.status | statusText}}</el-tag>
+            <span class="date">{{item.pubdate}}</span>
           </div>
         </div>
         <!-- 右侧内容 -->
@@ -68,8 +77,83 @@
 export default {
   data () {
     return {
-      list: [1, 2, 3, 5, 6]
+      // 搜索工具栏的数据
+      formData: {
+        status: 5, // 默认是全部
+        channelid: null, // 定义频道id 当前的选择的频道
+        dateRange: null // 时间范围 是个数组
+      },
+      channels: [], // 频道数据
+      list: [], // 内容列表
+      page: {
+        total: 0
+      }
     }
+  },
+  methods: {
+    // 刷新列表数据  状态改变/频道切换/日期改变 都会触发同一个事件
+    refreshList () {
+      // console.log(this.formData.status) // 看一下是不是最新的状态
+      let { status, channelid, dateRange } = this.formData // 把所有的解构出来
+      let params = {
+        // 三元表达式
+        status: status === 5 ? null : status, // 由于默认给了5,是5的话不能传,所以特殊处理
+        channel_id: channelid,
+        begin_pubdate: dateRange && dateRange.length ? dateRange[0] : null,
+        end_pubdate: dateRange && dateRange.length > 1 ? dateRange[1] : null
+      }
+      this.getArticles(params) // 在这调用下面的接口,把值传进去
+    },
+    // 获取文章
+    getArticles (params) {
+      this.$axios({
+        url: '/articles',
+        params: { ...params } // 把他解构进来
+      }).then(result => {
+        this.list = result.data.results // 将当前的数据赋值给data中的对象
+        this.page.total = result.data.total_count // 当前总条数
+      })
+    },
+    // 获取频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  // 过滤器,做状态显示的标签
+  filters: { // value是传过来的值
+    statusText (value) {
+      switch (value) {
+        case 0:
+          return '草稿'
+        case 2:
+          return '已发表'
+        case 3:
+          return '审核失败'
+        case 4:
+          return '已删除'
+      }
+    },
+    // 过滤tag标签的类型
+    statusType (value) {
+      switch (value) {
+        case 0:
+          return 'warning'
+        case 2:
+          return 'success'
+        case 3:
+          return 'danger'
+        case 4:
+          return 'info'
+      }
+    }
+  },
+  created () {
+    this.getArticles() // 获取文章
+    this.getChannels() // 获取频道
   }
 }
 </script>

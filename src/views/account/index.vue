baa37fbb08343e4dc9ac51c51e32633bd10db5ed
liulong <1362457117@qq.com>
2019-08-25 22:33:08 +0800

    账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,36 +6,59 @@
       <el-form label-width="100px">
           <!-- 用户名 -->
           <el-form-item label="用户名">
-              <el-input style="width:300px"></el-input>
+              <el-input v-model="formData.name" style="width:300px"></el-input>
           </el-form-item>
           <!-- 用户简介 -->
           <el-form-item label="用户简介">
-              <el-input style="width:300px"></el-input>
+              <el-input  v-model="formData.intro" style="width:300px"></el-input>
           </el-form-item>
           <!-- 邮箱 -->
          <el-form-item label="用户邮箱">
-              <el-input style="width:300px"></el-input>
+              <el-input v-model="formData.email" style="width:300px"></el-input>
           </el-form-item>
           <!-- 手机号 -->
           <el-form-item label="用户手机号">
-              <el-input style="width:300px" disabled></el-input>
+              <el-input  v-model="formData.mobile" style="width:300px" disabled></el-input>
           </el-form-item>
           <!-- 保存按钮 -->
           <el-form-item>
              <el-button type='primary'>保存信息</el-button>
          </el-form-item>
       </el-form>
-       <img class='head-img' src="../../assets/img/404.png" alt="">
+       <img class='head-img' :src="formData.photo? formData.photo:'imgUrl'" alt="">
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      imgUrl: require('../../assets/img/404.png'),
+      formData: {
+        name: '', // 用户名
+        intro: '', // 头条号简介
+        photo: '', // 用户头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
+      }
+    }
+  },
+  methods: {
+    getpersonal () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getpersonal()
+  }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
     .head-img {
      position: absolute;
      left: 700px;

f879d5d048cdf5db50f001e45ce81c80a8f73ff1
liulong <1362457117@qq.com>
2019-08-26 11:08:14 +0800

    -页面进度条引入应用

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,9 @@
 import router from './router'
+import nprogress from 'nprogress' // 引入文件
+import 'nprogress/nprogress.css' // 引入样式 //进度条插件
 // 全局导航,前置守卫
 router.beforeEach(function (to, from, next) {
+  nprogress.start() // 开始进度条
   // 如果以home起始,就认为进行需要检查token的区域
   if (to.path.startsWith('/home')) {
     // 获取前端的token
@@ -19,5 +22,9 @@ router.beforeEach(function (to, from, next) {
   } else {
     next() // 如果访问的不是/home开头的直接放行
   }
+})
+// 后置守卫
+router.afterEach(() => {
+  nprogress.done() // 结束进度条
 })
 export default router // 导出router 然后再入口那跟换一下router的引入文件
